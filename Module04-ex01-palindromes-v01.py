78ddce214979812e21e2ae45eadbe556e2cf5f93
Zaneta Grabowska <zaneta_grabowska@poczta.onet.pl>
2020-07-11 00:43:38 +0200

    Change printed comments for function

--- Module04-ex01-palindromes-v01.py
+++ Module04-ex01-palindromes-v01.py
@@ -1,6 +1,6 @@
 def palindrome(word):
   if word == word[::-1]:
-    print(f"The word {word} is a palindrome.")
+    print("True" + '\n' + f"The word {word} is a palindrome.")
   else:
-    print(f"The word {word} is not a palindrome")
+    print("False" + '\n' + f"The word {word} is not a palindrome")
 palindrome("SUMMER")
